0902d0424d2e7f56262da3e3952c3ab30e399e97
Lubos Zapotocny <zapotocnylubos@gmail.com>
2023-06-22 18:06:10 +0200

    Update build script for forked domjudge

--- docker/judgehost/chroot-and-tar.sh
+++ docker/judgehost/chroot-and-tar.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 # Usage: https://github.com/DOMjudge/domjudge/blob/main/misc-tools/dj_make_chroot.in#L58-L87
 /opt/domjudge/judgehost/bin/dj_make_chroot
 
+/opt/domjudge/judgehost/bin/dj_run_chroot "apt update && apt install mono-mcs mono-devel"
+
 cd /
 echo "[..] Compressing chroot"
 tar -czpf /chroot.tar.gz --exclude=/chroot/tmp --exclude=/chroot/proc --exclude=/chroot/sys --exclude=/chroot/mnt --exclude=/chroot/media --exclude=/chroot/dev --one-file-system /chroot
