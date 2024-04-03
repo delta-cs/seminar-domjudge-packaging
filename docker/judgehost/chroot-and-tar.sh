a6582aff8624e0b6b39465b1ca4afd568f7d71ee
Lubos Zapotocny <zapotocnylubos@gmail.com>
2024-04-03 14:12:31 +0000

    Add Rust and Kotlin

--- docker/judgehost/chroot-and-tar.sh
+++ docker/judgehost/chroot-and-tar.sh
@@ -19,6 +19,23 @@ set -euo pipefail
   && rm /tmp/dotnet-install.sh \
   && apt remove wget \
 "
+/opt/domjudge/judgehost/bin/dj_run_chroot "
+  apt update \
+  && apt install rustc \
+  && rustc -V \
+"
+/opt/domjudge/judgehost/bin/dj_run_chroot "
+  apt update \
+  && apt install wget unzip \
+  && wget -O /tmp/kotlin-compiler.zip https://github.com/JetBrains/kotlin/releases/download/v1.9.23/kotlin-compiler-1.9.23.zip \
+  && unzip /tmp/kotlin-compiler.zip -d /usr/lib/kotlin \
+  && ln -s /usr/lib/kotlin/kotlinc/bin/kotlinc /usr/bin/kotlinc \
+  && ln -s /usr/lib/kotlin/kotlinc/bin/kotlin /usr/bin/kotlin \
+  && kotlinc -version \
+  && kotlin -version \
+  && rm /tmp/kotlin-compiler.zip \
+  && apt remove wget unzip \
+"
 
 cd /
 echo "[..] Compressing chroot"
