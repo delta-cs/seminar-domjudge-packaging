5bf55d16c7960c3ce554d71ede7a6cdd41a07662
sipekdan <daniel.sipek@post.cz>
2026-09-03 08:37:37 +0200

    Add PHP and TypeScript runtimes to judgehost chroot

--- docker/judgehost/chroot-and-tar.sh
+++ docker/judgehost/chroot-and-tar.sh
@@ -49,6 +49,20 @@ set -euo pipefail
   && go version \
 "
 
+
+/opt/domjudge/judgehost/bin/dj_run_chroot "
+  apt update \
+  && apt install -y php-cli \
+  && php -v \
+"
+
+/opt/domjudge/judgehost/bin/dj_run_chroot "
+  apt update \
+  && apt install -y npm \
+  && npm install -g typescript \
+  && tsc -v \
+"
+
 cd /
 echo "[..] Compressing chroot"
 tar -czpf /chroot.tar.gz --exclude=/chroot/tmp --exclude=/chroot/proc --exclude=/chroot/sys --exclude=/chroot/mnt --exclude=/chroot/media --exclude=/chroot/dev --one-file-system /chroot
